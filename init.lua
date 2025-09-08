c024d59ee35dbf463d872aff911b98d6d32bd3b9
Patrick Wurster <pwurst@workHorse.Mordor>
2025-09-08 15:44:30 -0600

    changed neo-tree to ~/.cargo/bin

--- init.lua
+++ init.lua
@@ -2,29 +2,34 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+local home_ts = vim.fn.expand("~/.cargo/bin")
+if not string.find(vim.env.PATH or "", home_ts, 1, true) then
+	vim.env.PATH = home_ts .. ":" .. (vim.env.PATH or "")
+end
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-  end
-  vim.opt.rtp:prepend(lazypath)
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-  -- Core settings, keymaps, autocmds
-  require("config.options")
-  require("config.keymaps")
-  require("config.autocmds")
-  require("config.providers")
+-- Core settings, keymaps, autocmds
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.providers")
 
-  -- Load plugins from lua/plugins/* and force HTTPS for GitHub
-  require("lazy").setup({
-    spec = { import = "plugins" },
-    change_detection = { notify = false },
-    git = { url_format = "https://github.com/%s.git" },
-  })
+-- Load plugins from lua/plugins/* and force HTTPS for GitHub
+require("lazy").setup({
+	spec = { import = "plugins" },
+	change_detection = { notify = false },
+	git = { url_format = "https://github.com/%s.git" },
+})
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,15 +1,35 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
-    cmd = "Neotree",
-    keys = { { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Explorer (Neo-tree)" }, },
-    opts = {
-      window = { position = "left", width = 32 },
-      filesystem = {
-        filtered_items = { hide_dotfiles = false, hide_gitignored = true },
-      },
-    },
-  },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = "Neotree",
+		keys = {
+			{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Explorer (Neo-tree)" },
+		},
+		opts = {
+			window = {
+				position = "left",
+				width = 32,
+				mappings = {
+					["<CR>"] = "open",
+					["l"] = "open",
+					["h"] = "close_node",
+					["v"] = "open_vsplit", -- ← vertical split
+					["s"] = "open_split", -- ← horizontal split
+					["t"] = "open_tabnew",
+					-- optional: handle Alt-v if your terminal sends ESC+v
+					["<M-v>"] = "open_vsplit",
+					["<A-v>"] = "open_vsplit",
+				},
+			},
+			filesystem = {
+				filtered_items = { hide_dotfiles = false, hide_gitignored = true },
+			},
+		},
+	},
 }
