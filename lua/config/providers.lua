1c9b7435ed30fefa6be1f0a053d0bc758b7dd6c0
Patrick Wurster <pwurst@workHorse.Mordor>
2025-09-08 13:52:45 -0600

    set config path relative to config

--- lua/config/providers.lua
+++ lua/config/providers.lua
@@ -1,4 +1,6 @@
--- Use your actual path:
-vim.g.python3_host_prog = "./.pyenvs/nvim/bin/python3"
+-- Always resolve relative to your config directory:
+local cfg = vim.fn.stdpath("config")
+
+vim.g.python3_host_prog = cfg .. "./.pyenvs/nvim/bin/python3"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
