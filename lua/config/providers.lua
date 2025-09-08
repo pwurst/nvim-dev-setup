3d5bb4e72e90831294f7c0795c3f7caa0253cd62
Patrick Wurster <pwurst@workHorse.Mordor>
2025-09-08 14:02:28 -0600

    corrected relative path to .venvs

--- lua/config/providers.lua
+++ lua/config/providers.lua
@@ -1,6 +1,6 @@
 -- Always resolve relative to your config directory:
 local cfg = vim.fn.stdpath("config")
 
-vim.g.python3_host_prog = cfg .. "./.pyenvs/nvim/bin/python3"
+vim.g.python3_host_prog = cfg .. "/.pyenvs/nvim/bin/python3"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
