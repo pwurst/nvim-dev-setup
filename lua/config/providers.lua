a944a9d424f330378a08d72827d1f8ad7ee7efef
Patrick Wurster <pwurst@workHorse.Mordor>
2025-09-08 13:50:45 -0600

    set pyenv to relative path

--- lua/config/providers.lua
+++ lua/config/providers.lua
@@ -1,4 +1,4 @@
 -- Use your actual path:
-vim.g.python3_host_prog = "/home/pwurst/.config/nvim/.pyenvs/nvim/bin/python3"
+vim.g.python3_host_prog = "./.pyenvs/nvim/bin/python3"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
